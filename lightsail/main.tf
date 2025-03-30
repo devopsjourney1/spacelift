12a6f1304d8a40a0f2c11a602949b51acd1ec058
Brad <Brad@devopsbrad.com>
2025-03-30 15:13:01 -0700

    move to lightsail folder

---
+++ lightsail/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lightsail_instance
+resource "aws_lightsail_instance" "server" {
+  count             = var.instance_count
+  name              = "my-server-${count.index + 1}"
+  availability_zone = var.availability_zone
+  blueprint_id      = "ubuntu_22_04"
+  key_pair_name     = "id_rsa"
+  bundle_id         = "micro_3_0"
+  tags = {
+    environment = "development"
+  }
+  user_data = <<-EOF
+#!/bin/bash
+apt-add-repository ppa:ansible/ansible
+apt update
+apt install ansible -y
+EOF
+}
---
+++ lightsail/terraform.tfvars
@@ -0,0 +1,1 @@
+availability_zone = "us-west-2a"
