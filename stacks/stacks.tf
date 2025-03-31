b70c12171809e03a3b1861d94d238d2d0e1050cb
Brad <Brad@devopsbrad.com>
2025-03-30 20:24:40 -0700

    use custom role for stack

--- stacks/stacks.tf
+++ stacks/stacks.tf
@@ -9,11 +9,29 @@ resource "spacelift_stack" "lightsail-stack" {
 }
 
 
-resource "spacelift_aws_integration_attachment" "my_stack" {
-  integration_id = "spacelift"
-  stack_id       = "lightsail-servers"
-  read           = true
-  write          = true
+# IAM role.
+resource "aws_iam_role" "lightsail-stack" {
+  name = "lightsail-stack"
 
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      jsondecode(
+        spacelift_stack.managed-stack.aws_assume_role_policy_statement
+      )
+    ]
+  })
+}
+
+# Attaching a policy to it
+resource "aws_iam_role_policy_attachment" "managed-stack" {
+  role       = aws_iam_role.managed-stack.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
+
+# Telling Spacelift stack to assume it.
+resource "spacelift_stack_aws_role" "lightsail-stack" {
+  stack_id = spacelift_stack.lightsail-stack.id
+  role_arn = aws_iam_role.lightsail-stack.arn
 }
 
