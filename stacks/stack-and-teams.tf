44809440b20b1767a06ecfbc8ae766739fcc89b3
Brad <Brad@devopsbrad.com>
2025-03-30 19:31:24 -0700

    add stack

--- stacks/stack-and-teams.tf
+++ stacks/stack-and-teams.tf
@@ -6,18 +6,27 @@ resource "github_team" "stack-writers" {
   name = "managed-stack-writers"
 }
 
-resource "spacelift_stack" "managed-stack" {
-  name = "Stack managed by Spacelift"
+resource "spacelift_stack" "lightsail-stack" {
+  name = "lightsail-servers"
+  space_id = "root"
 
-  # Source code.
+  namespace = "devopsjourney1"
   repository = "spacelift"
-  branch     = "main"
+  branch = "main"
+  project_root = "lightsail"
 
+  terraform_version = "1.9.0"
+  terraform_workflow_tool = "OPEN_TOFU"
+  terraform_smart_sanitization = true
+
+  additional_project_globs = [""]
+  enable_well_known_secret_masking = true
+  github_action_deploy = false  
 }
 
 
 # Telling Spacelift stack to assume it.
-resource "spacelift_stack_aws_role" "managed-stack" {
+resource "spacelift_stack_aws_role" "lightsail-stack" {
   stack_id = spacelift_stack.managed-stack.id
   role_arn = "arn:aws:iam::688062348185:role/spacelift"
 }
