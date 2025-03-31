5a638c653c8264b7681abdd5b70f93b78e0cb358
Brad <Brad@devopsbrad.com>
2025-03-30 20:12:10 -0700

    testing

--- stacks/stacks.tf
+++ stacks/stacks.tf
@@ -3,7 +3,18 @@ resource "spacelift_stack" "lightsail-stack" {
   name = "lightsail-servers"
   space_id = "root"
 
+  namespace = "devopsjourney1"
   repository = "spacelift"
   branch = "main"
   project_root = "lightsail"
 }
+
+
+resource "spacelift_aws_integration_attachment" "my_stack" {
+  integration_id = "spacelift"
+  stack_id       = "lightsail-servers"
+  read           = true
+  write          = true
+
+}
+
