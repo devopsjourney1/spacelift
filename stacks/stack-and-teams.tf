8cd1e261468ac4a072847bbfa3e55670a3fb4707
Brad <Brad@devopsbrad.com>
2025-03-30 19:45:01 -0700

    remove namespace

--- stacks/stack-and-teams.tf
+++ stacks/stack-and-teams.tf
@@ -10,7 +10,6 @@ resource "spacelift_stack" "lightsail-stack" {
   name = "lightsail-servers"
   space_id = "root"
 
-  namespace = "devopsjourney1"
   repository = "spacelift"
   branch = "main"
   project_root = "lightsail"
