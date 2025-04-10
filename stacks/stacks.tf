b000f71d6c059901bf7367f5e413b4af8d71e420
Brad <Brad@devopsbrad.com>
2025-04-09 23:58:51 -0700

    add stack

--- stacks/stacks.tf
+++ stacks/stacks.tf
@@ -74,3 +74,81 @@ resource "spacelift_stack_aws_role" "eks-dev" {
   stack_id = spacelift_stack.eks-dev.id
   role_arn = aws_iam_role.eks-dev.arn
 }
+
+
+# ECR STack
+
+resource "spacelift_stack" "ecr-stack" {
+  name = "eks-stack"
+  space_id = "root"
+
+  repository = "spacelift"
+  branch = "main"
+  project_root = "ecr"
+}
+
+
+# IAM role.
+resource "aws_iam_role" "ecr-stack" {
+  name = "ecr-stack"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      jsondecode(
+        spacelift_stack.ecr-stack.aws_assume_role_policy_statement
+      )
+    ]
+  })
+}
+
+# Attaching a policy to it
+resource "aws_iam_role_policy_attachment" "ecr-stack" {
+  role       = aws_iam_role.ecr-stack.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
+
+# Telling Spacelift stack to assume it.
+resource "spacelift_stack_aws_role" "ecr-stack" {
+  stack_id = spacelift_stack.ecr-stack.id
+  role_arn = aws_iam_role.ecr-stack.arn
+}
+
+
+# Production servers
+
+resource "spacelift_stack" "production-stack" {
+  name = "eks-servers"
+  space_id = "root"
+
+  repository = "spacelift"
+  branch = "main"
+  project_root = "production"
+}
+
+
+# IAM role.
+resource "aws_iam_role" "production-stack" {
+  name = "production-stack"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      jsondecode(
+        spacelift_stack.production-stack.aws_assume_role_policy_statement
+      )
+    ]
+  })
+}
+
+# Attaching a policy to it
+resource "aws_iam_role_policy_attachment" "production-stack" {
+  role       = aws_iam_role.production-stack.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
+
+# Telling Spacelift stack to assume it.
+resource "spacelift_stack_aws_role" "production-stack" {
+  stack_id = spacelift_stack.production-stack.id
+  role_arn = aws_iam_role.production-stack.arn
+}
